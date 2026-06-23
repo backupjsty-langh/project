c62da460f0857eddf8644fbd62543f6a5d3dee1c
backupjsty-langh <backupj.sty@gmail.com>
2026-06-23 00:55:38 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# Cloudflare Pages build script
-# Set LOVABLE_URL in CF Pages environment variables (Settings > Environment Variables)
 
-if [ -z "$LOVABLE_URL" ]; then
-  echo "ERROR: LOVABLE_URL environment variable is not set."
+
+if [ -z "$MAIN_URL" ]; then
+  echo "ERROR: MAIN_URL environment variable is not set."
   exit 1
 fi
 
 echo "Injecting LOVABLE_URL..."
-sed -i "s|__LOVABLE_URL__|$LOVABLE_URL|g" index.html
-echo "Done. Deploying with URL: $LOVABLE_URL"
+sed -i "s|__MAIN_URL__|$MAIN_URL|g" index.html
+echo "Done. Deploying with URL: $MAIN_URL"
